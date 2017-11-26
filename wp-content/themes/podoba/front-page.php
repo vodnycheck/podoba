955e74806528ad8fe5c066f2523c8e594ab1d172
vodianitsky <vodianitsky@gmail.com>
2017-11-26 17:03:41 +0200

    gallery

--- wp-content/themes/podoba/front-page.php
+++ wp-content/themes/podoba/front-page.php
@@ -141,63 +141,28 @@ get_header(); ?>
 							<a name="-1" class="slide-change"></a>
 						</div>
 						<div class="container my-slides fad col-10">
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/810.jpg">
-	    						<img src="<?php echo $imageDir;?>small/810.jpg">
+							<?php
+								//Get the images ids from the post_metadata
+								$images = acf_photo_gallery('gallery', $post->ID);
+								//Check if return array has anything in it
+								if( count($images) ):
+									//Cool, we got some data so now let's loop over it
+									foreach($images as $image):
+										$id = $image['id']; // The attachment id of the media
+										$title = $image['title']; //The title
+										$caption= $image['caption']; //The caption
+										$full_image_url= $image['full_image_url']; //Full size image url
+										$full_image_url = acf_photo_gallery_resize_image($full_image_url, 262, 160); //Resized size to 262px width by 160px height image url
+										$thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
+										$url= $image['url']; //Goto any link when clicked
+										$target= $image['target']; //Open normal or new tab
+										$alt = get_field('photo_gallery_alt', $id); //Get the alt which is a extra field (See below how to add extra fields)
+										$class = get_field('photo_gallery_class', $id); //Get the class which is a extra field (See below how to add extra fields)
+							?>
+							<a data-fancybox="gallery" <a href="<?php echo $url; ?>">
+								<img src="<?php echo $full_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
 							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/798.jpg">
-	    						<img src="<?php echo $imageDir;?>small/798.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/829.jpg">
-	    						<img src="<?php echo $imageDir;?>small/829.jpg">
-							</a>
-							<a data-fancybox="gallery"  href="<?php echo $imageDir;?>big/856.jpg">
-	    						<img src="<?php echo $imageDir;?>small/856.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/789.jpg">
-	    						<img src="<?php echo $imageDir;?>small/789.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/771.jpg">
-	    						<img src="<?php echo $imageDir;?>small/771.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/282.jpg">
-	    						<img src="<?php echo $imageDir;?>small/282.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/910.jpg">
-	    						<img src="<?php echo $imageDir;?>small/910.jpg">
-							</a>
-							<a  data-fancybox="gallery" href="<?php echo $imageDir;?>big/290.jpg">
-	    						<img src="<?php echo $imageDir;?>small/290.jpg">
-							</a>
-								
-							<!--  -->
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/856.jpg">
-	    						<img src="<?php echo $imageDir;?>small/856.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/789.jpg">
-	    						<img src="<?php echo $imageDir;?>small/789.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/771.jpg">
-	    						<img src="<?php echo $imageDir;?>small/771.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/282.jpg">
-	    						<img src="<?php echo $imageDir;?>small/282.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/910.jpg">
-	    						<img src="<?php echo $imageDir;?>small/910.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/290.jpg">
-	    						<img src="<?php echo $imageDir;?>small/290.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/810.jpg">
-	    						<img src="<?php echo $imageDir;?>small/810.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/789.jpg">
-	    						<img src="<?php echo $imageDir;?>small/789.jpg">
-							</a>
-							<a data-fancybox="gallery" href="<?php echo $imageDir;?>big/829.jpg">
-	    						<img src="<?php echo $imageDir;?>small/829.jpg">
-							</a>
-							<!--  -->
+						<?php endforeach; endif; ?>
 						</div>
 						<div class="slider-buttons col-1 slideGallery">
 							<a name="1" class="slide-change"></a>
